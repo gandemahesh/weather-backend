16e62f4786c9365371d26881d68b613d78b2e439
LAPTOP-91L444GM\mahes <gandema@oregonstate.edu>
2022-02-06 20:41:54 -0800

    secured tokens

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const key = "eyJ1c2VyIjoicmFqYWxpbnYiLCJwYXNzd29yZCI6InBhc3N3b3JkIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.rPaPZqtAUvJfaEgZp7y44POhq84ZMpYgY9TPkHah_Gk"
+const key = "enter your key here"
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
